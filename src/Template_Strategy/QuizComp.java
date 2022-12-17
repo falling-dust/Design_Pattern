adaf1bba66edd656dc3318b5d1cbed3ead930c80
Ameshiro <512065954@qq.com>
2022-12-17 11:54:56 +0800

    1

---
+++ src/Template_Strategy/QuizComp.java
@@ -0,0 +1,103 @@
+package Template_Strategy;
+
+import Builder.Game;
+import Observer.ScoreSubject;
+import Servant.Preparation;
+import SimpleFactory.Athlete;
+import SimpleFactory.Cat;
+import State.StateCommon;
+import State.StateGood;
+import State.StatePerfect;
+import State.StateTired;
+
+import java.util.Random;
+
+public class QuizComp extends Game implements Preparation {
+    public QuizComp(String Name) {
+        super(Name);
+    }
+
+    @Override
+    public void gamePreparation() {
+        System.out.println("FourHundredMetersSwimming Game is preparing!");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("FourHundredMetersSwimming Game prepared!");
+    }
+
+    @Override
+    public void gameStart() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("FourHundredMetersSwimming Game Started!");
+        for (int i = 0; i < 8; ++i) {
+            getValue(i);
+        }
+        sortScore();
+    }
+
+    @Override
+    public void gameEnd() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("FourHundredMetersSwimming Game Finished!");
+        System.out.println("400m游泳最终成绩：");
+        setVisited(true);
+        for (int i = 1; i <= 8; ++i) {
+            System.out.println("参赛猫猫号码：" + i + "  名字：" + getCatLists().get(i - 1).getAnimalName() + "  成绩：" + getScore()[i - 1] + "秒");
+        }
+        System.out.println("第一名：" + getCatLists().get(getRankByRank()[0]).getAnimalName());
+        System.out.println("第二名：" + getCatLists().get(getRankByRank()[1]).getAnimalName());
+        System.out.println("第三名：" + getCatLists().get(getRankByRank()[2]).getAnimalName());
+        System.out.println("你的名次：" + getRankByNO()[0]);
+        ScoreSubject.getInstance().notifyObserver(2, getRankByNO(), getScore());
+        if (getRankByNO()[0] >= 6) {
+            switch (getCatLists().get(0).getAnimalState().toString()) {
+                case "StatePerfect":
+                    getCatLists().get(0).setAnimalState(new StateCommon());
+                    break;
+                case "StateGood":
+                    getCateLists().get(0).setAnimalState(new StateTired());
+                    break;
+                case "StateTired":
+                    break;
+                default:
+                    break;
+            }
+        } else if (getRankByNO()[0] == 1) {
+            switch (getCatLists().get(0).getAnimalState().toString()) {
+                case "StatePerfect":
+                    break;
+                case "StateGood":
+                    getCatLists().get(0).setAnimalState(new StatePerfect());
+                    break;
+                case "StateTired":
+                    getCatLists().get(0).setAnimalState(new StateCommon());
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void getValue(int no) {
+        Random rand = new Random();
+        double random = (rand.nextInt(2000) + 9000);
+        Cat athlete = getCatLists().get(no);
+        double value = (athlete.getSpeed() * 1.1 + athlete.getBeauty() * 1.2 + athlete.getIntelligence() * 1 + athlete.getSkill() * 1.3) / athlete.getAnimalState().getState();
+        double time = value * 0.5623183 * random / 10000;
+        getScore()[no] = time;
+    }
+
+
+}
